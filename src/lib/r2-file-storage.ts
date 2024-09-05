28c626ebdc91a7a40e03625d218c94e48c31d469
Sergio Xalambrí <hello@sergiodxa.com>
2024-09-05 15:01:10 -0400

    Move CustomMetada to R2FileStorage namespace

--- src/lib/r2-file-storage.ts
+++ src/lib/r2-file-storage.ts
@@ -1,9 +1,11 @@
 import type { R2Bucket } from "@cloudflare/workers-types";
 import type { FileStorage } from "@mjackson/file-storage";
 
-export interface CustomMetadata {
-	name: string;
-	type: string;
+export namespace R2FileStorage {
+	export interface CustomMetadata extends Record<string, string> {
+		name: string;
+		type: string;
+	}
 }
 
 /**
@@ -21,9 +23,14 @@ export class R2FileStorage implements FileStorage {
 	}
 
 	async set(key: string, file: File) {
+		let customMetadata = {
+			name: file.name,
+			type: file.type,
+		} satisfies R2FileStorage.CustomMetadata;
+
 		await this.r2.put(key, await file.arrayBuffer(), {
 			httpMetadata: { contentType: file.type },
-			customMetadata: { name: file.name, type: file.type },
+			customMetadata,
 		});
 	}
 
@@ -33,7 +40,8 @@ export class R2FileStorage implements FileStorage {
 
 		let buffer = await object.arrayBuffer();
 
-		let metadata = object.customMetadata as unknown as CustomMetadata;
+		let metadata =
+			object.customMetadata as unknown as R2FileStorage.CustomMetadata;
 
 		return new File([buffer], metadata?.name ?? key, {
 			type: object.httpMetadata?.contentType ?? metadata?.type,
